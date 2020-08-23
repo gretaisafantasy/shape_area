3d3b95b93741f183005089a5c1ae18cdfaf010bb
Greta Tanudjaja <53880303+gretaisafantasy@users.noreply.github.com>
2020-08-22 22:52:23 -0400

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,42 @@
+package mypackage;
+
+/**
+ *
+ * @author Greta Tanudjaja
+ */
+public class Circle extends Shape
+{
+    // Instance variable
+    private double mRadius;
+    
+    // A constructor that invokes the base constructor (passing the color) 
+    // and sets the radius instance value
+    public Circle(String color, double radius)
+    {
+        super(color);
+        mRadius = radius;
+    }
+    
+    // An overriding method that computes and returns the Circle object's
+    // area value
+    public double area()
+    {
+        return (3.14159 * mRadius * mRadius);
+    }
+    
+    // An overriding method that returns the Circle object's description
+    // (color, type, measurements, and area)
+    public String toString()
+    {
+        // Empty String 
+        String result = "";
+        
+        // Display the result of the Circle's description
+        result = getColor() + " circle with radius of " + mRadius
+                 + " and area of " + area();
+
+        // Return the result of the Circle's description
+        return result;
+    }
+            
+}
---
+++ Shape.java
@@ -0,0 +1,36 @@
+package mypackage;
+
+/**
+ *
+ * @author Greta Tanudjaja
+ */
+public class Shape
+{
+    // Instance variable
+    private String mColor;
+    
+    // A constructor that sets the color instance value
+    public Shape(String color)
+    {
+        mColor = color;
+    }
+    
+    // A method that returns the object's color value
+    public String getColor()
+    {
+        return mColor;
+    }
+    
+    // A method that computes and returns the object's area
+    public double area()
+    {
+        return 0.0;
+    }
+    
+    // A method that returns the object's description (color, type,
+    // measurements, and area) as a String
+    public String toString()
+    {
+        return "generic shape";
+    }
+}
